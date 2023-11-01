4502b20ffab09e8bedd5291632e7c6f50ac0cbbb
Nidhal Mouldi <mouldinidhal@gmail.com>
2023-11-01 22:30:59 +0100

    feat(home): Improve home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,19 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  articles = [
+  offers = [
     {name:'Robe', image: 'robe blue.jpg', prix: 10.5},
-    {name:'Robe', image: 'robe jaune.jpg', prix: 10.5},
-    {name:'Robe', image: 'robe noire.jpg', prix: 10.5},
-    {name:'Robe', image: 'jupe rouge.jpg', prix: 10.5},
+    {name:'Robe', image: 'robe jaune.jpg', prix: 14.5},
+    {name:'Robe', image: 'robe noire.jpg', prix: 17.5},
+    {name:'Robe', image: 'jupe rouge.jpg', prix: 18.5},
     {name:'Robe', image: 'pantalon beige.jpg', prix: 10.5},
     {name:'Robe', image: 'robe blue.jpg', prix: 10.5},
+    {name:'Robe', image: 'robe jaune.jpg', prix: 14.5},
+    {name:'Robe', image: 'robe noire.jpg', prix: 17.5},
+    {name:'Robe', image: 'jupe rouge.jpg', prix: 18.5},
+    {name:'Robe', image: 'pantalon beige.jpg', prix: 10.5},
+  ];
+
+  bestSellers = [
     {name:'Robe', image: 'robe blue.jpg', prix: 10.5},
+    {name:'Robe', image: 'robe jaune.jpg', prix: 14.5},
+    {name:'Robe', image: 'robe noire.jpg', prix: 17.5},
+    {name:'Robe', image: 'jupe rouge.jpg', prix: 18.5}
+  ];
+
+  bestArticles = [
     {name:'Robe', image: 'robe blue.jpg', prix: 10.5},
-    {name:'Robe', image: 'robe blue.jpg', prix: 10.5},
-    {name:'Robe', image: 'robe blue.jpg', prix: 10.5},
-    {name:'Robe', image: 'robe blue.jpg', prix: 10.5},
-    {name:'Robe', image: 'robe blue.jpg', prix: 10.5}
+    {name:'Robe', image: 'robe jaune.jpg', prix: 14.5},
+    {name:'Robe', image: 'robe noire.jpg', prix: 17.5},
+    {name:'Robe', image: 'jupe rouge.jpg', prix: 18.5}
   ];
 
 }
